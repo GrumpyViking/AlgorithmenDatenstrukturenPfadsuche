9a348aed11d2a986ebd7c0553d5abfd81f2c54af
Martin Schuster <37813025+GrumpyViking@users.noreply.github.com>
2018-12-20 22:08:41 +0100

    Add Auto-Rotating Camera

--- AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/CameraController.cs
+++ AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/CameraController.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+public class CameraController : MonoBehaviour {
     // Update is called once per frame
-    void Update()
-    {
-        
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public Vector3 desiredPosition;
+    public float radius = 2.0f;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
+    public void Start() {
+
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+        transform.LookAt(center);
+        radius = 2.0f;
+    }
+    void FixedUpdate() {
+        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+        desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
     }
 }
