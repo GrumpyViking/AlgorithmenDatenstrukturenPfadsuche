546ebf4fb86371cd17d6f2f28a0d08eab375586e
Martin Schuster <37813025+GrumpyViking@users.noreply.github.com>
2018-12-29 13:04:47 +0100

    add Spawnable Obstacles

--- AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/ComplexVersion/FollowMouse.cs
+++ AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/ComplexVersion/FollowMouse.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+    float speed = 200f;
     void Update()
     {
 		// Quelle: https://forum.unity.com/threads/solved-moving-gameobject-along-x-and-z-axis-by-drag-and-drop-using-x-and-y-from-screenspace.488476/#post-3185720
@@ -19,9 +20,17 @@ public class FollowMouse : MonoBehaviour
         if(plane.Raycast(ray, out distance))
         {
             draggingObject.position = ray.GetPoint(distance); // distance along the ray
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+                draggingObject.transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+                draggingObject.transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+            
         }
-		
-		
+        
+        
+
+    
         if (Input.GetMouseButtonDown(0))
         {
             Destroy(GetComponent<FollowMouse>());
--- AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/ComplexVersion/PlaceObjects.cs
+++ AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/ComplexVersion/PlaceObjects.cs
@@ -3,24 +3,46 @@ using UnityEngine.UI;
 
 public class PlaceObjects : MonoBehaviour
 {
-    public GameObject cube;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    public GameObject cube, cylinder;
+    private GameObject obstacle;
     
     public void PlaceObject(Text objectToPlace)
     {
         switch (objectToPlace.text)
         {
             case "Cube":
-                GameObject obstacle = Instantiate(cube, new Vector3(0, 0, 0), Quaternion.identity);
+                obstacle = Instantiate(cube, new Vector3(0, 0, 0), Quaternion.identity);
+                obstacle.layer = 30;
+                obstacle.GetComponent<Renderer>().material.color = Color.yellow;
+                obstacle.AddComponent<FollowMouse>();
+                break;
+            case  "LargeCube":
+                obstacle = Instantiate(cube, new Vector3(0, 0, 0),
+                    Quaternion.identity);
+                obstacle.transform.localScale += new Vector3(2f,0,2f);
+                obstacle.layer = 30;
+                obstacle.GetComponent<Renderer>().material.color = Color.yellow;
+                obstacle.AddComponent<FollowMouse>();
+                break;
+            case "Rectangle":
+                obstacle = Instantiate(cube, new Vector3(0, 0, 0),
+                    Quaternion.identity);
+                obstacle.transform.localScale += new Vector3(0,0,3f);
+                obstacle.layer = 30;
+                obstacle.GetComponent<Renderer>().material.color = Color.yellow;
+                obstacle.AddComponent<FollowMouse>();
+                break;
+            case "Cylinder":
+                obstacle = Instantiate(cylinder, new Vector3(0, 0, 0),
+                    Quaternion.identity);
+                obstacle.layer = 30;
+                obstacle.GetComponent<Renderer>().material.color = Color.yellow;
+                obstacle.AddComponent<FollowMouse>();
+                break;
+            case "LargeCylinder":
+                obstacle = Instantiate(cylinder, new Vector3(0, 0, 0),
+                    Quaternion.identity);
+                obstacle.transform.localScale += new Vector3(2f,0,2f);
                 obstacle.layer = 30;
                 obstacle.GetComponent<Renderer>().material.color = Color.yellow;
                 obstacle.AddComponent<FollowMouse>();
