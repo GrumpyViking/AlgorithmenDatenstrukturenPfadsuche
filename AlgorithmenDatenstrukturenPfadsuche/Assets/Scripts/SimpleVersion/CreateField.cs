ae0f6a247ae6829e740fcf3f446d7866e9bac721
Martin Schuster <37813025+MartinSchuster@users.noreply.github.com>
2019-01-11 23:45:34 +0100

    Preview for Loading Level

--- AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/SimpleVersion/CreateField.cs
+++ AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/SimpleVersion/CreateField.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 /*
 * CreatField Klasse erstellt das Spielfeld 
 */
-public class CreateField : MonoBehaviour {
+public class CreateField : MonoBehaviour
+{
     public GameObject fieldCell; // Objekt aus dem das Feldbesteht (in Unity hinzufügen)
     public Vector2 fieldSize; // Größe des Spielfeldes (in Unity eintragen)
     private float fieldCellSize = 0.5f; // größe der einzelnen Felder
@@ -21,12 +22,15 @@ public class CreateField : MonoBehaviour {
     public GameObject astarpanel, bfspanel, dfspanel; // Legenden für A*-Algoritmuse und Breitensuche Algoritmus
     public bool paused;
     public GameObject saveDialogPanel, loadDialogPanel;
+    public RenderTexture renderTexture;
+    public RawImage rawImage;
 
     public Dropdown levelList;
     /*
      * Initialisierung mit Programmstart
      */
-    void Start() {
+    void Start()
+    {
         fieldCellDiameter = fieldCellSize * 2;
         fieldSizeXAxis = Mathf.RoundToInt(fieldSize.x / fieldCellDiameter);
         fieldSizeYAxis = Mathf.RoundToInt(fieldSize.y / fieldCellDiameter);
@@ -37,13 +41,16 @@ public class CreateField : MonoBehaviour {
     /*
      * Erstellen des Spielfeldes
      */
-    private void CreateFieldGrid() {
+    private void CreateFieldGrid()
+    {
         fieldCellArray = new Node[fieldSizeXAxis, fieldSizeYAxis];
         Vector3 bottomLeft = transform.position - Vector3.right * fieldSize.x / 2 - Vector3.forward * fieldSize.y / 2; // Erstellung des Feldes um den Mittelpunkt anstelle des Mittlepunktes als Linken oberen Eckpunktes
         bool traversable = true;
         int counter = 0; // Zähler wie viele felder erstellt wurden 
-        for (int x = 0; x < fieldSizeXAxis; x++) {
-            for (int y = 0; y < fieldSizeYAxis; y++) {
+        for (int x = 0; x < fieldSizeXAxis; x++)
+        {
+            for (int y = 0; y < fieldSizeYAxis; y++)
+            {
                 Vector3 cordinate = bottomLeft + Vector3.right * (x * fieldCellDiameter + fieldCellSize) +
                                  Vector3.forward * (y * fieldCellDiameter + fieldCellSize); // Position an der neues Feld platziert wird
                 field = Instantiate(fieldCell, cordinate, Quaternion.identity); // Erstellt ein neues FeldObjekt an der zuvor festgelegten Position mit der standart Rotation
@@ -53,7 +60,7 @@ public class CreateField : MonoBehaviour {
                 field.GetComponent<Rigidbody>().isKinematic = true; // wichtig um Kollision unter den Felder zu vermeiden
                 fieldCellArray[x, y] = new Node(traversable, cordinate, x, y, field); // Fügt das aktuelle Feld dem array zu
                 fieldCellArray[x, y].index = counter;
-                 counter++;
+                counter++;
             }
         }
     }
@@ -61,7 +68,8 @@ public class CreateField : MonoBehaviour {
     /*
      * Interaktion durch den Benutzer um Start, Ziel und Hindernisse zu bestimmen
      */
-    void Update() {
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0) && !paused) // Linkemaustaste
         {
             Vector3 mouse = Input.mousePosition;
@@ -69,16 +77,68 @@ public class CreateField : MonoBehaviour {
             RaycastHit hit;
             if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) // Von der aktuellen Mausposition wird ein Strahl gesendet wenn dieser ein Objekt trifft wird eine der Aktionen ausgeführt
             {
-                if (!startSelected) {
+                if (!startSelected)
+                {
                     GameObject target = hit.rigidbody.gameObject; // Liefert das getroffene Objekt zurück
                     SetStart(target);
 
-                } else if (startSelected && !targetSelected) {
+                }
+                else if (startSelected && !targetSelected)
+                {
                     GameObject target = hit.rigidbody.gameObject;
                     SetTarget(target);
-                } else if (startSelected && targetSelected) {
+                }
+                else if (startSelected && targetSelected)
+                {
                     GameObject target = hit.rigidbody.gameObject;
                     SetBarricade(target);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1) && !paused)
+        {
+            Vector3 mouse = Input.mousePosition;
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) // Von der aktuellen Mausposition wird ein Strahl gesendet wenn dieser ein Objekt trifft wird eine der Aktionen ausgeführt
+            {
+                GameObject target = hit.rigidbody.gameObject;
+                RemoveStatus(target);
+            }
+        }
+
+        void RemoveStatus(GameObject field)
+        {
+            foreach (Node node in fieldCellArray)
+            {
+                if (node.fieldCell == field)
+                {
+                    if (node.start)
+                    {
+                        LevelData mnode = new LevelData(node);
+                        modifyedNodes.Remove(mnode);
+                        node.start = false;
+                        new ModifyNode().ChangeColor(node.fieldCell, Color.white);
+                        startSelected = false;
+                    }
+
+                    if (node.target)
+                    {
+                        LevelData mnode = new LevelData(node);
+                        modifyedNodes.Remove(mnode);
+                        node.target = false;
+                        new ModifyNode().ChangeColor(node.fieldCell, Color.white);
+                        targetSelected = false;
+                    }
+
+                    if (!node.traversable)
+                    {
+                        LevelData mnode = new LevelData(node);
+                        modifyedNodes.Remove(mnode);
+                        node.traversable = true;
+                        new ModifyNode().ChangeColor(node.fieldCell, Color.white);
+                    }
                 }
             }
         }
@@ -87,10 +147,13 @@ public class CreateField : MonoBehaviour {
     /*
      * Bestimmt das dass ausgewählte Feld eine Hindernis ist
      */
-    private void SetBarricade(GameObject field) {
+    private void SetBarricade(GameObject field)
+    {
         new ModifyNode().ChangeColor(field, Color.yellow);
-        foreach (Node node in fieldCellArray) {
-            if (field.transform.position == node.GetGlobalPosition()) {
+        foreach (Node node in fieldCellArray)
+        {
+            if (field.transform.position == node.GetGlobalPosition())
+            {
                 node.traversable = false; // Feld als nicht mehr begehbar markiert
                 LevelData mnode = new LevelData(node);
                 mnode.index = node.index;
@@ -102,10 +165,13 @@ public class CreateField : MonoBehaviour {
     /*
      * Bestimmt das dass ausgewählte Feld der Start ist
      */
-    void SetStart(GameObject field) {
+    void SetStart(GameObject field)
+    {
         new ModifyNode().ChangeColor(field, Color.green);
-        foreach (Node node in fieldCellArray) {
-            if (field.transform.position == node.GetGlobalPosition()) {
+        foreach (Node node in fieldCellArray)
+        {
+            if (field.transform.position == node.GetGlobalPosition())
+            {
                 node.start = true; // Feld wird als Startpunkt markiert
                 LevelData mnode = new LevelData(node);
                 mnode.index = node.index;
@@ -118,10 +184,13 @@ public class CreateField : MonoBehaviour {
     /*
      * Bestimmt das dass ausgewählte Feld das Ziel ist
      */
-    void SetTarget(GameObject field) {
+    void SetTarget(GameObject field)
+    {
         new ModifyNode().ChangeColor(field, Color.red);
-        foreach (Node node in fieldCellArray) {
-            if (field.transform.position == node.GetGlobalPosition()) {
+        foreach (Node node in fieldCellArray)
+        {
+            if (field.transform.position == node.GetGlobalPosition())
+            {
                 node.target = true; // Feld wird als Zielpunkt markiert
                 LevelData mnode = new LevelData(node);
                 mnode.index = node.index;
@@ -132,14 +201,16 @@ public class CreateField : MonoBehaviour {
     }
 
     // Gibt das Felderarray an andere Klassen zurück
-    public Node[,] GetArray() {
+    public Node[,] GetArray()
+    {
         return fieldCellArray;
     }
 
     /*
      * Liefert die Nachbarfelder eines Feldes als Liste zurück
      */
-    public List<Node> GetNeighboringNodes(Node current) {
+    public List<Node> GetNeighboringNodes(Node current)
+    {
         List<Node> neighbors = new List<Node>();
         int checkXAxis; // prüfen ob Feld noch im gültigen X-Achsen Rahmen ist 
         int checkYAxis; // prüfen ob Feld noch im gültigen Y-Achsen Rahmen ist
@@ -147,8 +218,10 @@ public class CreateField : MonoBehaviour {
         //oberer-Nachbar
         checkXAxis = current.cordX;
         checkYAxis = current.cordY + 1;
-        if (checkXAxis >= 0 && checkXAxis < fieldSizeXAxis) {
-            if (checkYAxis >= 0 && checkYAxis < fieldSizeYAxis) {
+        if (checkXAxis >= 0 && checkXAxis < fieldSizeXAxis)
+        {
+            if (checkYAxis >= 0 && checkYAxis < fieldSizeYAxis)
+            {
                 neighbors.Add(fieldCellArray[checkXAxis, checkYAxis]);
             }
         }
@@ -156,8 +229,10 @@ public class CreateField : MonoBehaviour {
         //rechter-Nachbar
         checkXAxis = current.cordX + 1;
         checkYAxis = current.cordY;
-        if (checkXAxis >= 0 && checkXAxis < fieldSizeXAxis) {
-            if (checkYAxis >= 0 && checkYAxis < fieldSizeYAxis) {
+        if (checkXAxis >= 0 && checkXAxis < fieldSizeXAxis)
+        {
+            if (checkYAxis >= 0 && checkYAxis < fieldSizeYAxis)
+            {
                 neighbors.Add(fieldCellArray[checkXAxis, checkYAxis]);
             }
         }
@@ -165,8 +240,10 @@ public class CreateField : MonoBehaviour {
         //unterer-Nachbar
         checkXAxis = current.cordX;
         checkYAxis = current.cordY - 1;
-        if (checkXAxis >= 0 && checkXAxis < fieldSizeXAxis) {
-            if (checkYAxis >= 0 && checkYAxis < fieldSizeYAxis) {
+        if (checkXAxis >= 0 && checkXAxis < fieldSizeXAxis)
+        {
+            if (checkYAxis >= 0 && checkYAxis < fieldSizeYAxis)
+            {
                 neighbors.Add(fieldCellArray[checkXAxis, checkYAxis]);
             }
         }
@@ -174,8 +251,10 @@ public class CreateField : MonoBehaviour {
         //linker-Nachbar
         checkXAxis = current.cordX - 1;
         checkYAxis = current.cordY;
-        if (checkXAxis >= 0 && checkXAxis < fieldSizeXAxis) {
-            if (checkYAxis >= 0 && checkYAxis < fieldSizeYAxis) {
+        if (checkXAxis >= 0 && checkXAxis < fieldSizeXAxis)
+        {
+            if (checkYAxis >= 0 && checkYAxis < fieldSizeYAxis)
+            {
                 neighbors.Add(fieldCellArray[checkXAxis, checkYAxis]);
             }
         }
@@ -186,8 +265,10 @@ public class CreateField : MonoBehaviour {
     /*
      * Anzeige der richtigen Legende zum gewählten Algoritmus
      */
-    public void ShowPanel(Text panel) {
-        switch (panel.text) {
+    public void ShowPanel(Text panel)
+    {
+        switch (panel.text)
+        {
             case "A* - Algorithmus":
                 print("A*");
                 GameObject.Find("GameManager").GetComponent<AStarAlgorithmAlt>().enabled = true;
@@ -222,13 +303,27 @@ public class CreateField : MonoBehaviour {
     public void SaveLevel(Text name)
     {
         SaveSystem.levelName = name.text;
+        ScreenShot(name.text);
         SaveSystem.SaveData(modifyedNodes);
         saveDialogPanel.SetActive(false);
         paused = false;
     }
 
-
-    public void ShowSaveDialog()
+    void ScreenShot(string filename)
+    {
+        string savePath = Application.dataPath + "/levels";
+        byte[] bytes = toTexture2D(renderTexture).EncodeToPNG();
+        System.IO.File.WriteAllBytes(savePath + "/" + filename + ".png", bytes);
+    }
+    Texture2D toTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(300, 300, TextureFormat.RGB24, false);
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
+    void ShowSaveDialog()
     {
         saveDialogPanel.SetActive(true);
         paused = true;
@@ -238,23 +333,21 @@ public class CreateField : MonoBehaviour {
     {
         string filePath = Application.dataPath + "/levels";
         DirectoryInfo dir = new DirectoryInfo(filePath);
-        FileInfo[] info = dir.GetFiles("*.grid");
+        FileInfo[] names = dir.GetFiles("*.grid");
         levelList.options.Clear();
-        foreach (FileInfo f in info)
+        foreach (FileInfo f in names)
         {
             levelList.options.Add(new Dropdown.OptionData(f.Name));
         }
-        
         loadDialogPanel.SetActive(true);
         paused = true;
     }
-    
 
     public void LoadLevel(Text filename)
     {
         ClearGrid();
         SavableData savedLevel = SaveSystem.LoadLevel(filename.text);
-        
+
         foreach (Node node in fieldCellArray)
         {
             foreach (LevelData ld in savedLevel.saveNodes)
@@ -297,7 +390,27 @@ public class CreateField : MonoBehaviour {
             node.hCost = Int32.MaxValue;
             node.visited = false;
             new ModifyNode().ChangeColor(node.fieldCell, Color.white);
+        }
+    }
 
+    public void ChangePreview(Text name)
+    {
+        string filePath = Application.dataPath + "/levels";
+        Texture2D preview;
+        string filename;
+        byte[] bytes;
+        DirectoryInfo dir = new DirectoryInfo(filePath);
+        FileInfo[] images = dir.GetFiles("*.png");
+        foreach (FileInfo i in images)
+        {
+            if (i.Name.Substring(0, i.Name.Length - 5) == name.text.Substring(0, name.text.Length - 6))
+            {
+                preview = new Texture2D(300, 300, TextureFormat.RGB24, false);
+                filename = i.Name;
+                bytes = File.ReadAllBytes(Application.dataPath + "/levels/" + filename);
+                preview.LoadImage(bytes);
+                rawImage.texture = preview;
+            }
         }
     }
 }
