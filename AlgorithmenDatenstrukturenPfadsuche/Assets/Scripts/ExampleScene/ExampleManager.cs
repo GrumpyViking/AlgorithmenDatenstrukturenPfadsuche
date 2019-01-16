43c2ab389b008b42e6a35f7940f3ad51e43aaf94
Martin Schuster <37813025+mschuster@users.noreply.github.com>
2019-01-16 09:55:44 +0100

    add Description A*, BFS, DFS to Learnmode

--- AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/ExampleScene/ExampleManager.cs
+++ AlgorithmenDatenstrukturenPfadsuche/Assets/Scripts/ExampleScene/ExampleManager.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ExampleManager : MonoBehaviour {
-
-    void Start() {
+    void Awake() {
         switch (PlayerSceneData.lastScene) {
-
+            case 5:
+                Debug.Log("Tiefensuche");
+                break;
+            case 6:
+                Debug.Log("Breitensuche");
+                break;
+            case 7:
+                Debug.Log("A*");
+                break;
+            default:
+                Debug.Log("Fehler im ExampleMode!");
+                break;
         }
     }
 
